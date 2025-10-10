a2fdbe0e9df0fe85af906987ca851d867494ef6c
gab271 <gabrielmedinahernandez@alumno.ieselrincon.es>
2025-10-10 18:23:03 +0100

    layout done

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>My Application</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Una App de Bicicleta</h1>
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
